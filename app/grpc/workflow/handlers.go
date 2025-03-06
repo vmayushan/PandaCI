aec5afd48e238a1b87cd90bc00053ef6f8497cf2
Alfie Jones <alfie.jones@hotmail.co.uk>
2025-03-06 21:16:08 +0000

    improving errors

--- app/grpc/workflow/handlers.go
+++ app/grpc/workflow/handlers.go
@@ -3,6 +3,7 @@ package grpcWorkflow
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"strconv"
 	"time"
@@ -116,8 +117,17 @@ func (h *Handler) StartTask(ctx context.Context, req *connect.Request[pb.Workflo
 		Id:           createdTask.Msg.Id,
 	}))
 	if err != nil {
-		log.Error().Err(err).Msg("starting task")
-		h.addWorkflowRunAlert(ctx, pb.WorkflowAlert_TYPE_ERROR, "Failed to start task", err.Error())
+
+		msg := ""
+		if connectErr := new(connect.Error); errors.As(err, &connectErr) {
+			msg = connectErr.Message()
+			log.Error().Err(err).Str("msg", msg).Any("connectErr", connectErr).Msg("starting task connect error")
+		} else {
+			msg = err.Error()
+			log.Error().Err(err).Msg("starting task")
+		}
+
+		h.addWorkflowRunAlert(ctx, pb.WorkflowAlert_TYPE_ERROR, "Failed to start task", msg)
 		return nil, err
 	}
 
