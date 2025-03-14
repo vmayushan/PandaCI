fd88e73eed848067cc253727f6fe5b8bd54a4cea
Alfie Jones <alfie.jones@hotmail.co.uk>
2025-03-14 18:29:55 +0000

    updating wrangler usage

--- .pandaci/cleanup.workflow.ts
+++ .pandaci/cleanup.workflow.ts
@@ -25,13 +25,13 @@ await docker("node:22", { name: "Cleanup fly" }, async () => {
 
   $`flyctl apps delete ${flyEnv.FLY_APP} --yes`.env(flyEnv);
 
-  $`npx wrangler kv:key delete ${env.PANDACI_BRANCH} --namespace-id=${env.CLOUDFLARE_KV_NAMESPACE_ID}`
+  $`npx wrangler kv:key delete ${env.PANDACI_BRANCH} --remote --namespace-id=${env.CLOUDFLARE_KV_NAMESPACE_ID}`
     .env({
       CLOUDFLARE_API_TOKEN: env.CLOUDFLARE_API_TOKEN,
       CLOUDFLARE_ACCOUNT_ID: env.CLOUDFLARE_ACCOUNT_ID,
     });
 
-  $`npx wrangler kv:key delete ${env.PANDACI_BRANCH} --namespace-id=${env.CLOUDFLARE_API_KV_ID}`
+  $`npx wrangler kv:key delete ${env.PANDACI_BRANCH} --remote --namespace-id=${env.CLOUDFLARE_API_KV_ID}`
     .env({
       CLOUDFLARE_API_TOKEN: env.CLOUDFLARE_API_TOKEN,
       CLOUDFLARE_ACCOUNT_ID: env.CLOUDFLARE_ACCOUNT_ID,
