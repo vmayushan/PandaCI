e252f51799693e808f906136c38ba663dec01e33
Alfie Jones <alfie.jones@hotmail.co.uk>
2025-03-17 22:00:13 +0000

    Update welcome page layout and content

--- web/src/routes/welcome/+page.svelte
+++ web/src/routes/welcome/+page.svelte
@@ -2,8 +2,8 @@
 	import { queries } from '$lib/queries';
 	import { createQuery } from '@tanstack/svelte-query';
 	import CreateOrgForm, { type CreateOrgFormData } from './createOrgForm.svelte';
-	import { Button, Card, Heading, Text, Divider, TextLink } from '$lib/components';
-	import { X } from 'phosphor-svelte';
+	import { Button, Card, Heading, Text, Divider, TextLink, DialogActions } from '$lib/components';
+	import { SkipBack, X } from 'phosphor-svelte';
 	import { goto } from '$app/navigation';
 	import { useCreateOrgMutation } from '../account/useCreateOrg';
 	import { handleForm } from '$lib/utils';
@@ -12,7 +12,7 @@
 
 	$effect(() => {
 		if (orgs.data && orgs.data.length) {
-			goto('/');
+			// goto('/');
 		}
 	});
 
@@ -21,13 +21,10 @@
 	const createOrgMutation = useCreateOrgMutation();
 </script>
 
-<div class="flex h-full min-h-svh flex-col overflow-auto py-8">
-	<Card class="relative m-auto w-full max-w-md">
-		<div class="absolute right-4 top-4">
-			<Button href="/" plain class=""><X class="h-4 w-4" /></Button>
-		</div>
+<div class="sm:bg-surface-low flex h-full min-h-svh flex-col overflow-auto py-8">
+	<Card hideMobileRing class="relative m-auto w-full max-w-md !ring-0 sm:ring-1">
 		<Heading class="mb-2">Create your organization</Heading>
-		<Text>Set up your organization's workspace on our platform.</Text>
+		<Text>Set up your organization's (or personal) workspace on our platform.</Text>
 
 		{#if createOrgMutation.error}
 			<Text variant="error">{createOrgMutation.error.message}</Text>
@@ -44,16 +41,15 @@
 			<Button loading={createOrgMutation.isPending} color="dark/white" full type="submit">
 				Create organization
 			</Button>
+
+			<Text class="text-center">
+				Want to join an org? <TextLink href="https://pandaci.com/docs/platform/other/org-members">
+					Get an invite
+				</TextLink>
+			</Text>
 		</form>
+		<Divider class="my-4" />
 
-		<Divider class="mb-12 mt-8" />
-
-		<Text class="text-center">
-			Want to join an organization? <TextLink
-				href="https://pandaci.com/docs/platform/other/org-members"
-			>
-				Ask your admin for an invite
-			</TextLink>
-		</Text>
+		<Button full plain>Skip</Button>
 	</Card>
 </div>
