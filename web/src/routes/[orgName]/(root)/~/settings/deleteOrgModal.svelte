bc4e7d3b6e517d1b4db4ab060b43d20bdb122cbf
Alfie Jones <alfie.jones@hotmail.co.uk>
2025-03-18 19:05:10 +0000

    adding delete org modal

---
+++ web/src/routes/[orgName]/(root)/~/settings/deleteOrgModal.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import {
+		Dialog,
+		DialogTitle,
+		DialogActions,
+		Text,
+		Button,
+		DialogDescription
+	} from '$lib/components';
+	import type { Organization } from '$lib/api/organization';
+	import { API } from '$lib/api';
+	import { createMutation, useQueryClient } from '@tanstack/svelte-query';
+	import { queries } from '$lib/queries';
+	import DialogCloseButton from '$lib/components/dialog/dialogCloseButton.svelte';
+
+	interface DeleteOrgModal {
+		org: Organization;
+		open: boolean;
+	}
+
+	let { org, open = $bindable() }: DeleteOrgModal = $props();
+
+	const queryClient = useQueryClient();
+
+	const deleteOrgMutation = createMutation(() => ({
+		mutationFn: () =>
+			API.delete('/v1/orgs/{orgSlug}', {
+				params: { orgSlug: org.slug }
+			}),
+		onSettled: () => {
+			queryClient.invalidateQueries(queries.organization.list());
+			queryClient.setQueryData(queries.organization.getByName(org.slug).queryKey, undefined);
+		}
+	}));
+</script>
+
+<Dialog bind:open>
+	<DialogTitle>Delete <i>{org?.name}</i></DialogTitle>
+	<DialogDescription>
+		Are you sure you want to delete this organization? This action cannot be undone.
+	</DialogDescription>
+	{#if deleteOrgMutation.error}
+		<Text variant="error">{deleteOrgMutation.error.message}</Text>
+	{/if}
+	<form
+		class="flex flex-col space-y-8"
+		onsubmit={(e) => {
+			deleteOrgMutation.mutate(undefined, {
+				onSuccess: () => {
+					(e.target as HTMLFormElement)?.reset();
+					open = false;
+				}
+			});
+		}}
+	>
+		<DialogActions>
+			<DialogCloseButton plain>Cancel</DialogCloseButton>
+			<Button loading={deleteOrgMutation.isPending} color="red" class="self-end" type="submit">
+				Delete Organization
+			</Button>
+		</DialogActions>
+	</form>
+</Dialog>
